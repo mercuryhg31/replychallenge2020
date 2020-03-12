10c8aeb3d4c216b8293ec9019a5d19aaa10fd4eb
Harshita Garg <26150304+mercuryhg31@users.noreply.github.com>
2020-03-12 12:23:33 -0400

    added teams to case

--- p1/Case.java
+++ p1/Case.java
@@ -6,6 +6,7 @@ public class Case {
     private int numOfTeams;
     private int numOfLogs;
     private ArrayList<Log> logs;
+    private ArrayList<Team> teams;
 
     public Case(int numOfTeams, int numOfLogs, ArrayList<Log> logs) {
         this.numOfTeams = numOfTeams;
@@ -23,6 +24,10 @@ public class Case {
 
     public ArrayList<Log> getLogs() {
         return logs;
+    }
+
+    public ArrayList<Team> getTeams() {
+        return teams;
     }
 
 }
