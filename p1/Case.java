82729a29390c9ae010802ade92dfb023196eea64
demitech1024 <demitech1024@gmail.com>
2020-03-12 12:20:26 -0400

    Created Case.java file and created getters as well as a construtor

---
+++ p1/Case.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Case {
+    private int numOfTeams;
+    private int numOfLogs;
+    private ArrayList<Log> logs;
+
+    public Case(int numOfTeams, int numOfLogs, ArrayList<Log> logs) {
+        this.numOfTeams = numOfTeams;
+        this.numOfLogs = numOfLogs;
+        this.logs = logs;
+    }
+    
+    public int getNumOfTeams() {
+        return numOfTeams;
+    }
+
+    public int getNumOfLogs() {
+        return numOfLogs;
+    }
+
+    public ArrayList<Log> getLogs() {
+        return logs;
+    }
+
+}
